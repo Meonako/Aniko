5262ee3554304f086b4d994b303fdf9aef96d39d
Meonako <dubi.db28@gmail.com>
2022-12-04 22:46:56 +0700

    Remove log mesages

--- utils/decode-image.go
+++ utils/decode-image.go
@@ -7,17 +7,11 @@ import (
 	"github.com/Meonako/go-logger/v2"
 )
 
-var i = 0
-
 func DecodeBase64ToImage(base64Data string) *bytes.Reader {
-	logger.ToTerminal("Start Decode Base64: ", i)
 	decoded, err := base64.StdEncoding.DecodeString(base64Data)
 	if err != nil {
 		logger.ToTerminalRed("Cannot decode base64: ", err)
 		return nil
 	}
-
-	i++
-	logger.ToTerminal("Finished Decode Base64: ", i)
 	return bytes.NewReader(decoded)
 }
